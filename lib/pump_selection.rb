edc5d1af318f83d6ef7d0e61df0fe8a9a9676069
Salma Elshahawy <salmaeng71@gmail.com>
2018-03-14 22:32:35 -0400

    change the order of require

--- lib/pump_selection.rb
+++ lib/pump_selection.rb
@@ -1,13 +1,8 @@
+rrequire_relative "./pump_selection/version"
+require_relative "./pump_selection/cli"
+require_relative "./pump_selection/series"
+
 require 'open-uri'
 require 'pry'
 require 'nokogiri'
-require 'mechanize'
 require 'colorize'
-
-# require_relative "./pump_selection/scrape"
-require_relative "./pump_selection/series"
-require_relative "./pump_selection/cli"
-require_relative "./pump_selection/version"
-# module PumpSelection
-#   # Your code goes here...
-# end
